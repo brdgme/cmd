45b19ab521d1eff4cb3755060d8500bc19a90dd7
Michael Alexander <beefsack@gmail.com>
2016-08-25 23:30:06 +1000

    Using simplified ANSI interface

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 extern crate brdgme_markup;
 
-use brdgme_markup::parse;
-use brdgme_markup::render::ansi;
+use brdgme_markup::ansi;
 
 fn main() {
     println!("{}",
-             ansi::render(&parse("blah {{#b}}blah{{/b}} blah {{#bg red}}{{#fg \
-                                  green}}CHRISTMAS{{/fg}}{{/bg}} {{player 0}} {{player 1}}")
-                              .unwrap(),
-                          vec!["mick".to_string(), "steve".to_string()])
+             ansi("blah {{#b}}blah{{/b}} blah {{#bg red}}{{#fg \
+                                  green}}CHRISTMAS{{/fg}}{{/bg}} {{player 0}} {{player 1}}",
+                  vec!["mick".to_string(), "steve".to_string()])
                  .unwrap());
 
 }
