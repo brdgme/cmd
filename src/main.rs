8f604888e00367b639b9d4b56a01b28ad52f47b2
Michael Alexander <beefsack@gmail.com>
2016-08-25 22:57:09 +1000

    Test with ANSI renderer

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
-extern crate brdgme_color;
+extern crate brdgme_markup;
+
+use brdgme_markup::parse;
+use brdgme_markup::render::ansi;
 
 fn main() {
-    for (i, c) in brdgme_color::player_colors().iter().enumerate() {
-        println!("{}Player {}",
-                 brdgme_color::Style { fg: c.mono().inv(), bg: *c, bold: true }.ansi(),
-                 i+1,
-        );
-    }
-    for (i, c) in brdgme_color::player_colors().iter().enumerate() {
-        println!("{}Player {}",
-                 brdgme_color::Style { fg: *c, bg: brdgme_color::WHITE, bold: true }.ansi(),
-                 i+1,
-        );
-    }
+    println!("{}",
+             ansi::render(&parse("blah {{#b}}blah{{/b}} blah {{#bg red}}{{#fg \
+                                  green}}CHRISTMAS{{/fg}}{{/bg}} {{player 0}} {{player 1}}")
+                              .unwrap(),
+                          vec!["mick".to_string(), "steve".to_string()])
+                 .unwrap());
+
 }
