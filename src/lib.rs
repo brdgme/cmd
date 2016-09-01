26c38edb16705a7cd3640e686dcee20e1b195203
Michael Alexander <beefsack@gmail.com>
2016-09-01 20:58:02 +1000

    Improved prompt

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ extern crate brdgme_game;
 extern crate brdgme_markup;
 extern crate brdgme_color;
 
-use std::io;
+use std::io::{self, Write};
 use std::fmt::Debug;
 
 use brdgme_game::{Gamer, Renderer, Commander, Log, GameError};
@@ -16,10 +16,7 @@ pub fn repl<T>(game: &mut T)
     print!("{}", Style::default().ansi());
     let mut players: Vec<String> = vec![];
     loop {
-        output(&format!("Enter player {} (or blank to finish):", players.len() + 1));
-        let mut player = String::new();
-        io::stdin().read_line(&mut player).unwrap();
-        player = player.trim().to_owned();
+        let player = prompt(&format!("Enter player {} (or blank to finish)", players.len() + 1));
         if player == "" {
             break;
         }
@@ -33,20 +30,24 @@ pub fn repl<T>(game: &mut T)
             return;
         }
         let current_player = turn[0];
-        output(&format!("\n{}\n\nEnter command for {}:",
-                        ansi(&game.render(Some(current_player)).unwrap(), &players).unwrap(),
-                        ansi(&vec![N::Player(current_player)], &players).unwrap()));
-
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
-        input = input.trim().to_owned();
+        output(&format!("\n{}\n",
+                        ansi(&game.render(Some(current_player)).unwrap(), &players).unwrap()));
+        let input = prompt(&format!("Enter command for {}",
+                                    ansi(&vec![N::Player(current_player)], &players).unwrap()));
         match input.as_ref() {
             ":dump" | ":d" => output(&format!("{:#?}", game)),
             ":quit" | ":q" => return,
             _ => {
                 match game.command(current_player, &input, &players) {
                     Ok(l) => output_logs(l, &players),
-                    Err(GameError::InvalidInput(desc)) => output(&desc),
+                    Err(GameError::InvalidInput(desc)) => {
+                        output(&ansi(&vec![N::Bold(vec![N::Fg(brdgme_color::RED,
+                                                              vec![
+                            N::Text(desc),
+                                                 ])])],
+                                     &players)
+                            .unwrap())
+                    }
                     Err(e) => panic!(e),
                 }
             }
@@ -70,4 +71,12 @@ fn output(s: &str) {
                  .map(|l| format!("{}\x1b[K", l))
                  .collect::<Vec<String>>()
                  .join("\n"));
+}
+
+fn prompt(s: &str) -> String {
+    print!("{}: \x1b[K", s);
+    io::stdout().flush().unwrap();
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    input.trim().to_owned()
 }
